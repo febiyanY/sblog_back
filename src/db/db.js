8f8f789314889630f1eb5ca901e67205654793fa
febiYanY <yfebiyan@gmail.com>
2020-08-29 08:35:26 +0700

    change db env var

--- src/db/db.js
+++ src/db/db.js
@@ -1,17 +1,21 @@
 import {Sequelize} from 'sequelize'
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host : process.env.DB_HOST,
-    dialect : process.env.DB_DIALECT,
-    pool : {
-        max :5,
-        min : 0,
-        acquire : 30000,
-        idle : 10000
-    },
-    define : {
-        timestamps : false
-    },
+// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     host : process.env.DB_HOST,
+//     dialect : process.env.DB_DIALECT,
+//     pool : {
+//         max :5,
+//         min : 0,
+//         acquire : 30000,
+//         idle : 10000
+//     },
+//     define : {
+//         timestamps : false
+//     },
+//     logging : false
+// })
+
+const db = new Sequelize(process.env.DATABASE_URL,{
     logging : false
 })
 
