f75f792002ef924a8d6d54c9626383e12514191a
febiYanY <yfebiyan@gmail.com>
2020-09-06 08:32:21 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-const express = require('express') 
-const http = require('http') 
-const cors = require('cors') 
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
 const path = require('path')
-const {sequelize : db} = require('./models/index') 
+const { sequelize: db } = require('./models/index')
 const session = require('express-session')
 const connect_session_sequelize = require('connect-session-sequelize')
 
 const SequelizeStore = connect_session_sequelize(session.Store)
 
 var SessionStore = new SequelizeStore({
-    db : db
+    db: db
 })
 
-const r_users = require('./routes/users') 
-const r_posts = require('./routes/posts') 
-const r_comments = require('./routes/comments') 
+const r_users = require('./routes/users')
+const r_posts = require('./routes/posts')
+const r_comments = require('./routes/comments')
 
 const c_users = require('./controllers/users')
 
@@ -24,7 +24,21 @@ const publicPath = path.join(__dirname, '../public')
 const corsOptions = {
     origin: process.env.CORS_ORIGIN,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
-    credentials : true
+    credentials: true
+}
+const sessConfig = {
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 2,
+    },
+    store: SessionStore
+}
+
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    sessConfig.cookie.secure = true // serve secure cookies
 }
 
 
@@ -32,17 +46,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(publicPath))
 app.use(cors(corsOptions))
-app.use(session({
-    secret : process.env.SESS_SECRET,
-    resave : false,
-    saveUninitialized : false,
-    proxy : true,
-    cookie : {
-        maxAge : 1000 * 60 * 60 * 24 * 2,
-        secure : true
-    },
-    store : SessionStore
-}))
+app.use(session(sessConfig))
 SessionStore.sync()
 
 app.use('/users', r_users)
