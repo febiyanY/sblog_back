df453b1501732abdba2bc9cd0e5ee2dfd723731b
febiYanY <yfebiyan@gmail.com>
2020-09-06 08:16:13 +0700

    test cookie

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(session({
     saveUninitialized : false,
     cookie : {
         maxAge : 1000 * 60 * 60 * 24 * 2,
-        secure : true
+        // secure : true
     },
     store : SessionStore
 }))
