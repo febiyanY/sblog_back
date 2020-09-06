81424ebcc74d0209157c5b0092ce186a9afe5e86
febiYanY <yfebiyan@gmail.com>
2020-09-06 08:07:10 +0700

    fix cookie secure

--- src/app.js
+++ src/app.js
@@ -37,7 +37,8 @@ app.use(session({
     resave : false,
     saveUninitialized : false,
     cookie : {
-        maxAge : 1000 * 60 * 60 * 24 * 2
+        maxAge : 1000 * 60 * 60 * 24 * 2,
+        secure : process.env.SESS_SECURITY === 'false' ? false : true
     },
     store : SessionStore
 }))
